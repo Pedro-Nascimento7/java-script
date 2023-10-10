866d4c08fac923db7129d590259ecfb99a863413
Pedro Nascimento <140713733+Pedro-Nascimento7@users.noreply.github.com>
2023-10-10 20:58:19 -0300

    .

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 22
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('Voto opicional')
+} else if (idade < 18 || idade >65) {
+}else {
+    console.log('Voto obrigatório')
+}
